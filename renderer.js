980178f898f77a17abcaa8dd89b08e9944a3c13a
Silent Garden <silentgarden837@gmail.com>
2025-04-03 16:44:17 +0530

    changing html button color and renderer js file import changing to common JS

--- renderer.js
+++ renderer.js
@@ -1,16 +1,17 @@
-import { ipcRenderer } from "electron"
 
 let fileChooser = document.getElementById('fileChooser')
 let convertButton = document.getElementById('conversionButton')
 let format = document.getElementById('format')
 
-convertButton.style.backgroundColor = 'green'
-
 fileChooser.addEventListener('change', () => {
   convertButton.disabled = false
 })
 
-//
-//ipcRenderer.on('file-path-response', (event, path) =>   {
-//  format.innerText = `File Path : ${path}`
-//})
+document.addEventListener('DOMContentLoaded', () => {
+  const { ipcRenderer } = require('electron')
+
+  ipcRenderer.on('file-path-response', (event, path) =>   {
+    format.innerText = `File Path : ${path}`
+  })
+})
+
